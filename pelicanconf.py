43c7faa5f806be7fc450fcbb67d5507b7e0650ef
Sergey Chizhik <s.s.chizhik@gmail.com>
2017-08-10 13:11:44 +0300

    Добавил ссылки на профили

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Sergey Chizhik'
-SITENAME = 'My Site'
+SITENAME = 's.chizhik #site'
 SITEURL = ''
 
 PATH = 'content'
@@ -20,16 +20,23 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+    ('You can modify those links in your config file', '#'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('github', 'https://github.com/s-chizhik'),
+    ('stack-overflow', 'https://stackoverflow.com/users/4469537/sergey-chizhik'),
+    ('linkedin', 'https://www.linkedin.com/in/sergey-chizhik-848b02141'),
+    ('telegram', 'https://t.me/s_chizhik'),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+THEME = 'themes/clean-blog'
